f40164431700c5b44aa096bc2ab20c8e05a745e7
byNoeRV <droldanvivanco@gmail.com>
2025-11-05 13:56:34 -0600

    Codigo terminado del programa 2a

---
+++ code/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        String inFile = "App.java";  
+        String outFile = "salida.txt";
+        Logic2 logic = new Logic2("Program2-LOC", "  ");
+        logic.logic2a(inFile, outFile);
+    }
+}
